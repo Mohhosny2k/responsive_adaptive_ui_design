435e1094f7602d11742c2ef4c3546fb86c97ea86
mohhosny2k <92595555+Mohhosny2k@users.noreply.github.com>
2024-04-08 00:34:05 +0300

    drawer header

--- lib/views/widgets/custom_drawer.dart
+++ lib/views/widgets/custom_drawer.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Drawer(
+      backgroundColor: Color(0xffDBDBDB),
+      child: Column(
+        children: [
+          DrawerHeader(child: Icon(FontAwesomeIcons.solidHeart,size:48)),
+        ]
+      ),
+    );
   }
 }
