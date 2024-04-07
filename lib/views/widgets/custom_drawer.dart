6cf978214e59e124d1a71ba0f546f2c6016d585d
mohhosny2k <92595555+Mohhosny2k@users.noreply.github.com>
2024-04-08 01:37:11 +0300

    fix error

--- lib/views/widgets/custom_drawer.dart
+++ lib/views/widgets/custom_drawer.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:responsive_adaptive_ui_design/views/widgets/custom_drawer_item.dart';
-
 import '../../models/drawer_item_model.dart';
 import 'custom_drawer_items_list_view.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
-  final List<DrawerItemModel> items = const [
+//final            const
+// static const 
+  static const List<DrawerItemModel> items =  [
     DrawerItemModel(title: 'D A S H B O A R D', icon: FontAwesomeIcons.solidHeart),
     DrawerItemModel(title: 'S E T T I N G S', icon: Icons.settings),
     DrawerItemModel(title: 'A B O U T', icon: Icons.info),
     DrawerItemModel(title: 'L O G O U T', icon: Icons.logout),
   ];
+
   @override
   Widget build(BuildContext context) {
     return  Drawer(
