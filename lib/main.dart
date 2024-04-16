ca8f0b1aca72df54bd76021fa068696ec1b8059c
mohhosny2k <92595555+Mohhosny2k@users.noreply.github.com>
2024-04-16 14:37:54 +0200

    finish  fun  ///  responsive text part 8

--- lib/main.dart
+++ lib/main.dart
@@ -50,16 +50,19 @@ class MyApp extends StatelessWidget {
 
 double getResponsiveFontSize(BuildContext context, {required double fontSize}) {
   double scalefactor = getScaleFactor(context);
-  double responsiveFontSize= fontSize * scalefactor;
+  double responsiveFontSize = fontSize * scalefactor;
+  double lowerLimit = fontSize * .8;
+  double upperLimit = fontSize * 1.2;
+return  responsiveFontSize.clamp(lowerLimit, upperLimit);
 }
 
 double getScaleFactor(BuildContext context) {
   double width = MediaQuery.sizeOf(context).width;
-    if (width < 600) {
-          return width/ 400;
-        }  else if(width < 900){
-          return  width/ 700;
-        } else {
-          return   width/1000;
-        }
+  if (width < 600) {
+    return width / 400;
+  } else if (width < 900) {
+    return width / 700;
+  } else {
+    return width / 1000;
+  }
 }
